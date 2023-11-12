3fff3b302f24f0366dd6ecae35fe289fe6f2fbc7
Osmel Martinez <69587351+CrowCrack23@users.noreply.github.com>
2023-11-12 02:19:00 -0500

    add auth module

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,58 @@
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import * as bcrypt from 'bcrypt';
+import { CreateUserDto } from './dto/create-user.dto';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import { LoginUserDto } from './dto/login-user.dto';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+  async create(createUserDto: CreateUserDto) {
+    try {
+      const { password, ...userData } = createUserDto;
+      const user = this.userRepository.create({
+        ...userData,
+        password: bcrypt.hashSync(password, 10),
+      });
+
+      await this.userRepository.save(user);
+      delete user.password;
+      return user;
+      // TODO: Rerturn access JWT
+    } catch (error) {
+      this.handleDBErrors(error);
+    }
+  }
+  async login(loginUserDto: LoginUserDto) {
+    const { password, email } = loginUserDto;
+    const user = await this.userRepository.findOne({
+      where: { email },
+      select: { email: true, password: true },
+    });
+    if (!user) {
+      throw new UnauthorizedException('Credentials are not valid (email)');
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      throw new UnauthorizedException('Credentials are not valid (password)');
+    }
+    return user;
+    // TODO: Return JWT
+  }
+
+  private handleDBErrors(error: any): never {
+    if (error.code === '23505') throw new BadRequestException(error.detail);
+    console.log(error);
+
+    throw new InternalServerErrorException('Please check server logs');
+  }
+}
